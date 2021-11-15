32e70fb0be6c0adc65591cde66d2c29ab93dbeb2
Vudang <danglegiavu@gmail.com>
2021-11-15 17:36:20 +0700

    setup typeorm with postgresql

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import * as Joi from '@hapi/joi';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { PostsModule } from './posts/posts.module';
+
+@Module({
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      })
+    }),
+    PostsModule,
+    DatabaseModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
